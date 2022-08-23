e5f151c60788abe87d5d78559efef0c38c70478c
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-08-23 11:25:32 +0300

    using find

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(record){
+    const recordFound = record.find(element => element.result === "W")
+    if (recordFound){
+        return recordFound.year
+    }else{
+        return undefined
+    }
+}
+
